386b289e136a6755a4c30aa245fbcf8f8c15f897
KimInJoong <ijboym216@gmail.com>
2024-04-23 17:07:09 +0900

    FullStack 앱 - LangChain FullStack 애플리케이션 기반 LLM 구축

--- ice_breaker-1-start-here/ice_breaker.py
+++ ice_breaker-1-start-here/ice_breaker.py
@@ -1,28 +1,49 @@
-# -*- coding: utf-8 -*-
 from dotenv import load_dotenv
-from langchain.chains import LLMChain
+from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+from output_parsers import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    load_dotenv()
-    print("Hello Langchain!")
-    summary_template ="""
-    이 링크드인에 대한 정보{information}를 가지고 두가지를 만들어줘
-    1. 간단한 요약
-    2. 그것에 대한 두가지 흥미로운 사실
+from typing import Tuple
+
+
+def ice_break_with(name: str) -> Tuple[Summary, str]:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
+
+    summary_template = """
+    given the information about a person from linkedin {information} I want you to create:
+    1. A short summary
+    2. two interesting facts about them 
+    3. A topic that may interest them
+    4. 2 creative Ice breakers to open a conversation with them
+    \n{format_instructions}
+
+    Use both information from Linkedin
     """
-
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
-
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
+    )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
-    )
+    res = chain.invoke(input={"information": linkedin_data})
 
-    print(chain.invoke(input={"information": linkedin_data}))
+
+    return res['information'], linkedin_data.get("profile_pic_url")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break_with(name="Harrison Chase")

19ec5d20b75dae03c3c2bf4fbafdf5f6bd15571a
KimInJoong <ijboym216@gmail.com>
2024-04-25 12:37:05 +0900

    AI 랭체인 대화 어시스턴트 구축하기 - 벡터 스토어 Pincone 수집

---
+++ documentation-helper/ingestion.py
@@ -0,0 +1,27 @@
+import os
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Pinecone
+from langchain_openai.embeddings import OpenAIEmbeddings
+def ingest_docs() -> None:
+    loader = ReadTheDocsLoader(path="/Users/admin/LLM-study/documentation-helper/langchain-docs/langchain.readthedocs.io/en/latest")
+    raw_documents = loader.load()
+    print(f"loaded {len(raw_documents)} documents")
+    text_spliter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""])
+    documents = text_spliter.split_documents(documents=raw_documents)
+    print(f"Splitted into {len(documents)} chunks")
+
+    for doc in documents:
+        old_path = doc.metadata["source"]
+        new_url = old_path.replace("langchain-docs", "https:/")
+        doc.metadata.update({"source": new_url})
+
+    print(f"Going to insert {len(documents)} to Pinecone")
+
+    embeddings = OpenAIEmbeddings()
+    Pinecone.from_documents(documents, embeddings, index_name="langchain-doc-index")
+    print("******* Added to Pinecone vectorstore vectors")
+
+
+if __name__ == "__main__":
+    ingest_docs()

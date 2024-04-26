7a431015bf91dbc40e37b6d0f8f8fe67033dd291
KimInJoong <ijboym216@gmail.com>
2024-04-26 10:39:43 +0900

    AI 랭체인 대화 어시스턴트 구축하기 - 메모리 및 ConversationalRetrievalChain

--- documentation-helper/backend/core.py
+++ documentation-helper/backend/core.py
@@ -1,6 +1,6 @@
-from typing import Any
+from typing import Any, List, Dict
 
-from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_community.vectorstores.pinecone import Pinecone
@@ -8,17 +8,18 @@ from langchain_community.vectorstores.pinecone import Pinecone
 from consts import INDEX_NAME
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Dict[str, Any]]) -> Any:
     embeddings = OpenAIEmbeddings()
     dosearch = Pinecone.from_existing_index(index_name=INDEX_NAME, embedding=embeddings)
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
+
+    qa = ConversationalRetrievalChain.from_llm(
         llm=chat,
-        chain_type="stuff",
         retriever=dosearch.as_retriever(),
         return_source_documents=True,
     )
-    return qa({"query": query})
+    return qa({"question": query, "chat_history": chat_history})
+
 
 if __name__ == "__main__":
     print(run_llm(query="What is LangChain?"))
--- documentation-helper/main.py
+++ documentation-helper/main.py
@@ -14,6 +14,9 @@ if "user_prompt_history" not in st.session_state:
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
 
 def create_source_string(source_url: Set[str]) -> str:
     if not source_url:
@@ -28,16 +31,26 @@ def create_source_string(source_url: Set[str]) -> str:
 
 
 if prompt:
-    with st.spinner(text='Generating response ...'):
-        generated_response = run_llm(query=prompt)
-        sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
+    with st.spinner(text="Generating response ..."):
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
+        sources = set(
+            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+        )
 
-        formatted_response = f"{generated_response['result']} \n\n {create_source_string(sources)}"
+        formatted_response = (
+            f"{generated_response['answer']} \n\n {create_source_string(sources)}"
+        )
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
 if st.session_state["chat_answers_history"]:
-    for generated_response, user_query in zip(st.session_state["chat_answers_history"], st.session_state["user_prompt_history"]):
+    for generated_response, user_query in zip(
+        st.session_state["chat_answers_history"],
+        st.session_state["user_prompt_history"],
+    ):
         message(user_query, is_user=True)
         message(generated_response)
